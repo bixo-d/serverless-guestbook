5e9a91640a9bce2a32b4a942a9ba0258ed0f50f1
Rafael Aiquel Garbarini <rafael.aiquel@gmail.com>
2022-07-04 22:56:15 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://489f12dc.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
